a305c624d5e7260f05bae00578609bfbeb206f7f
Eduardo San Martin Morote <posva13@gmail.com>
2014-03-28 20:18:46 +0100

    Texture manager

---
+++ TextureManager.cpp
@@ -0,0 +1,46 @@
+#include "TextureManager.h"
+
+std::map<std::string, TextureManager::GLImg> TextureManager::m_images;
+
+TextureManager::TextureManager()
+{}
+
+TextureManager::~TextureManager()
+{
+    for (std::map<std::string, GLImg>::iterator it(m_images.begin()); it != m_images.end(); ++it) {
+        glDeleteTextures(1, &it->second.id);
+    }
+    m_images.clear();
+}
+
+
+GLuint TextureManager::loadTexture(const QString &file, const std::string &key, bool smooth)
+{
+    QImage i;
+    if (m_images.find(key) != m_images.end()) {
+        std::cerr<<"Duplicate key '"<<key<<"' when loading "<<file.toStdString()<<"\n";
+        return 0;
+    }
+    if (i.load(file)) {
+        m_images[key].img = QGLWidget::convertToGLFormat(i);
+
+        //construire les textures openGL
+        glGenTextures( 1, &m_images[key].id );
+        std::cerr<<"Generated texture with id "<<m_images[key].id<<" from "<<file.toStdString()<<"\n";
+        glBindTexture( GL_TEXTURE_2D, m_images[key].id );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, smooth?GL_LINEAR:GL_NEAREST );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, smooth?GL_LINEAR:GL_NEAREST );
+
+        //les deux commandes suivantes servent a elargir la texture pq. la surface soit remplie;
+        //.. clamp_to_edge = repeter la derniere ligne de la texture (marche bien ici)
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, m_images[key].img.width(), m_images[key].img.height(), 0,
+                GL_RGBA, GL_UNSIGNED_BYTE, m_images[key].img.bits() );
+    } else {
+        std::cerr<<"Error loading file "<<file.toStdString()<<"\n";
+        return 0;
+    }
+
+    return m_images[key].id;
+}
---
+++ TextureManager.h
@@ -0,0 +1,40 @@
+#ifndef __TEXTUREMANAGER_H__
+#define __TEXTUREMANAGER_H__
+/*******************************************************************************
+ *  TextureManager                                                             *
+ *  Fri Mar 28 CET 2014                                                        *
+ *  Copyright Eduardo San Martin Morote                                        *
+ *  eduardo.san-martin-morote@ensimag.fr                                       *
+ *  http://posva.net                                                           *
+ ******************************************************************************/
+
+#include <map>
+#include <QGLViewer/qglviewer.h>
+#include <qimage>
+
+class TextureManager {
+		struct GLImg {
+			GLuint id;
+			QImage img;
+			GLImg() {}
+		};
+
+		static std::map<std::string, GLImg> m_images;
+
+		TextureManager();
+		~TextureManager();
+	
+	public:
+		static GLuint loadTexture(const QString &file, const std::string &key, bool smooth = true);
+
+		// On vérifie pas si la clé existe! C'est fait exprès
+		inline static GLuint getTexture(const std::string &key) {
+			return m_images[key].id;
+		}
+
+		inline static void bindTexture(const std::string &key) {
+			glBindTexture(GL_TEXTURE_2D, m_images[key].id);
+		}
+};
+
+#endif
